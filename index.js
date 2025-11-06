b91ef45b5e5264b951fe592b7bf1503960b3df17
talyssonfernandesweg-wq <talysson.fernandes.weg@gmail.com>
2025-11-06 12:46:41 -0300

    client

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const __dirname = path.resolve(path.dirname(new URL(import.meta.url).pathname));
 
 const app = express();
 app.use(cors());
-app.use(express.static(path.join(__dirname, "cliente")));
+app.use(express.static(path.join(__dirname, "client")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "cliente", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 const server = http.createServer(app);
